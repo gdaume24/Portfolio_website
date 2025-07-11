5dd50a10f7875412b2fec0ad9ea69e327c1bb7ca
gdaumer24 <geoffroy.daumer@outlook.fr>
2025-07-12 00:57:06 +0200

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Formations } from './formations/formations';
+import { Projets } from './projets/projets';
+import { Footer } from './footer/footer';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Formations, Projets, Footer, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'portfolio';
+  private m_projectsActive = false;
+
+  public Projects(): void {
+    this.m_projectsActive = !this.m_projectsActive;
+  }
+
+  public get ProjectsActive(): boolean {
+    return this.m_projectsActive;
+  }
 }
---
+++ src/app/formations/formations.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Projets } from "../projets/projets";
+
+@Component({
+  selector: 'app-formations',
+  imports: [Projets],
+  templateUrl: './formations.html',
+  styleUrl: './formations.css'
+})
+export class Formations {
+
+}
