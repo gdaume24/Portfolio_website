8f7e3c2144e8fd197d7887ceacaf31c720d816ab
gdaume24 <geoffroy.daumer@outlook.fr>
2026-07-08 10:20:43 +0000

    Fix CI headless test launcher

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,26 @@
 module.exports = function (config) {
   config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage')
+    ],
+    client: {
+      clearContext: false
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/portfolio'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }]
+    },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['ChromeHeadlessNoSandbox'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
@@ -13,6 +34,7 @@ module.exports = function (config) {
           '--disable-breakpad'
         ]
       }
-    }
+    },
+    restartOnFileChange: true
   });
 };
